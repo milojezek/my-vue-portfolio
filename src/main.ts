dab99f9e750fbf9572e01f9d36acfebf370d62ca
Miloslav Jezek <miloslav.jezek@fpt.sk>
2025-08-14 10:48:22 +0200

    Uninstall oh vue icons

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import "./assets/styles.css";
 
-import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { BiGithub, BiLinkedin } from "oh-vue-icons/icons";
 import { createApp } from "vue";
 
 import App from "./App.vue";
 
-addIcons(BiLinkedin, BiGithub);
-
 const app = createApp(App);
-app.component("v-icon", OhVueIcon);
 app.mount("#app");
