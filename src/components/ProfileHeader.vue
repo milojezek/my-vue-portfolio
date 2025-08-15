b41d7ae9ce4313603920b55f0c8c28c0c840d529
Miloslav Jezek <milo.jezek@gmail.com>
2025-08-15 22:11:43 +0200

    Move CSS styling exclusive to ProfileHeader to the component's file

--- src/components/ProfileHeader.vue
+++ src/components/ProfileHeader.vue
@@ -43,3 +43,125 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 300px;
+  position: relative;
+  z-index: 10;
+}
+
+header h1,
+header h4 {
+  color: var(--color-highlight);
+}
+
+header a {
+  text-decoration: none;
+  /* Remove the underline */
+}
+
+header h1 {
+  font-weight: 600;
+}
+
+header h4 {
+  font-weight: 400;
+}
+
+nav {
+  display: none;
+  font-weight: bold;
+}
+
+nav a {
+  font-size: 0.9rem;
+}
+
+.socials {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+}
+
+.social-icon {
+  width: 50px;
+  height: auto;
+  transition: transform 0.2s;
+}
+
+.social-icon:hover {
+  transform: scale(1.15);
+}
+
+@media (min-width: 640px) {
+  header {
+    max-width: unset;
+  }
+
+  header h1 {
+    font-size: 3rem;
+  }
+
+  header h4 {
+    font-size: 1.4rem;
+  }
+
+  header p {
+    max-width: 300px;
+  }
+
+  .socials {
+    flex-direction: row;
+    gap: 1.5rem;
+  }
+}
+
+@media (min-width: 1024px) {
+  header {
+    position: sticky;
+    top: 0;
+    margin-left: auto;
+    height: 100vh;
+    padding-bottom: 6rem;
+    padding-top: 6rem;
+    max-width: 600px;
+    width: 100%;
+  }
+
+  header h1 {
+    font-size: 3rem;
+  }
+
+  header h4 {
+    font-size: 1.4rem;
+  }
+
+  header p {
+    max-width: 300px;
+  }
+
+  nav {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    gap: 1rem;
+  }
+
+  nav a p {
+    font-size: 1rem;
+  }
+
+  .socials {
+    flex-direction: row;
+    gap: 1.5rem;
+  }
+
+  .socials p {
+    font-size: 2rem;
+  }
+}
+</style>
