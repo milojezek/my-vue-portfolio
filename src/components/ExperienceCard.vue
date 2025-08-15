cbfe3783f9ca89a8a58b380635fb1bcf75cb6b96
Miloslav Jezek <milo.jezek@gmail.com>
2025-08-15 22:53:48 +0200

    Move CSS styling exclusive to ExperienceCard to the component's file

--- src/components/ExperienceCard.vue
+++ src/components/ExperienceCard.vue
@@ -46,4 +46,74 @@ export default {
 </script>
 
 <style scoped>
+.experience-card {
+  display: grid;
+  /* Number of columns displayed in grid*/
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  gap: 0.5rem;
+}
+
+.experience-card h3 {
+  text-transform: uppercase;
+  font-size: 1rem;
+  font-weight: 500;
+}
+
+.experience-card p {
+  font-size: 0.9rem;
+}
+
+.card-column {
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+}
+
+.tenure {
+  font-size: 0.5rem !important;
+  opacity: 0.7;
+  text-transform: uppercase;
+}
+
+.card-header {
+  font-weight: 400;
+  font-size: 1rem;
+}
+
+.tag-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  color: var(--color-accent);
+}
+
+.tag {
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.75rem;
+  background: var(--color-highlight);
+}
+
+.tag p {
+  font-size: 0.7rem;
+}
+
+@media (min-width: 640px) {
+  .experience-card {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+
+  .card-column {
+    grid-column: span 3 / span 3;
+  }
+
+  .tenure {
+    font-size: 0.75rem !important;
+  }
+}
+
+@media (min-width: 1024px) {
+  .tenure {
+    font-size: 0.6rem !important;
+  }
+}
 </style>
